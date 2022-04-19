5aaf1170b370194d10af482cb7d559bbf55c2256
Glitch (rift-wandering-flag) <none>
2022-04-19 20:22:48 +0000

    🏛🖇 Checkpoint ./index.js:25920309/9839 ./LICENSE.md:25920309/1065 ./package.json:25920309/769

--- index.js
+++ index.js
@@ -2,176 +2,224 @@
 const express = require("express");
 const app = new express();
 const bodyParser = require("body-parser");
-const { Client , MessageEmbed } = require('discord.js');
+const { Client, MessageEmbed } = require("discord.js");
 const client = new Client({ intents: 32767 });
 client.login(process.env.token);
-client.on("ready",async () => { console.log("Hazır"); });
-const cors=require('cors');
+client.on("ready", async () => {
+  console.log("Hazır");
+});
+const cors = require("cors");
 const { Database } = require("ark.db");
 const db = new Database();
 //TANIMLAR
 
-
 //EXPRESS SERVER AYARLAR
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({origin:true,credentials: true}));
-app.use(express.static(__dirname + "/out"))
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.static(__dirname + "/out"));
 
 //EXPRESS SERVER AYARLAR
-app.get("/panel/:pass",(req,res) => {
-  if(req.params.pass == process.env.pass) res.sendFile(__dirname+"/index.html");
+app.get("/panel/:pass", (req, res) => {
+  if (req.params.pass == process.env.pass)
+    res.sendFile(__dirname + "/index.html");
   else res.send("Yanlış şifre");
 });
 
-
 // BOT
-let kufurliste = ["aq","amk","sik","sg","oç","piç","puşt","orospu","kahpe"];
-let reklamliste = ["https://","http://","com","www"];
+let kufurliste = [
+  "aq",
+  "amk",
+  "sik",
+  "sg",
+  "oç",
+  "piç",
+  "puşt",
+  "orospu",
+  "kahpe",
+];
+let reklamliste = ["https://", "http://", "com", "www"];
 
-client.on("messageCreate",message => {
-	if(kufurliste.some(k => message.content.toLowerCase().split(" ").includes(k)) && db.get("kufurEngel")) message.delete().catch(err => {  });
-	else if(reklamliste.some(k => message.content.toLowerCase().includes(k)) && db.get("reklamEngel")) message.delete().catch(err => {  });
+client.on("messageCreate", (message) => {
+  if (
+    kufurliste.some((k) =>
+      message.content.toLowerCase().split(" ").includes(k)
+    ) &&
+    db.get("kufurEngel")
+  )
+    message.delete().catch((err) => {});
+  else if (
+    reklamliste.some((k) => message.content.toLowerCase().includes(k)) &&
+    db.get("reklamEngel")
+  )
+    message.delete().catch((err) => {});
 });
 
-client.on("channelDelete",async channel => {
-	let fetchedLogs = await channel.guild.fetchAuditLogs({
-		limit: 1,
-		type: 'CHANNEL_DELETE',
-	});
+client.on("channelDelete", async (channel) => {
+  let fetchedLogs = await channel.guild.fetchAuditLogs({
+    limit: 1,
+    type: "CHANNEL_DELETE",
+  });
 
-	let deletionLog = fetchedLogs.entries.first();
+  let deletionLog = fetchedLogs.entries.first();
 
-	let m = `${String(new Date()).split(" ")[4]} ${channel.name} isimli kanal silindi.`;
+  let m = `${String(new Date()).split(" ")[4]} ${
+    channel.name
+  } isimli kanal silindi.`;
 
-	if(!deletionLog) m += `Kimin sildiği bilinmiyor`;
-	else {
-		let { executor, target } = deletionLog;
-		m += `Silen kişi : ${executor.tag}`;
-	};
+  if (!deletionLog) m += `Kimin sildiği bilinmiyor`;
+  else {
+    let { executor, target } = deletionLog;
+    m += `Silen kişi : ${executor.tag}`;
+  }
 
-	db.push("logs",m);
+  db.push("logs", m);
 
-	if(db.get("kanalKoruma")){
-		channel.clone();
-	};
+  if (db.get("kanalKoruma")) {
+    channel.clone();
+  }
 });
 
-client.on("roleDelete",async role => {
-	let fetchedLogs = await role.guild.fetchAuditLogs({
-		limit: 1,
-		type: 'ROLE_DELETE',
-	});
+client.on("roleDelete", async (role) => {
+  let fetchedLogs = await role.guild.fetchAuditLogs({
+    limit: 1,
+    type: "ROLE_DELETE",
+  });
 
-	let deletionLog = fetchedLogs.entries.first();
+  let deletionLog = fetchedLogs.entries.first();
 
-	let m = `${String(new Date()).split(" ")[4]} ${role.name} isimli rol silindi.`;
+  let m = `${String(new Date()).split(" ")[4]} ${
+    role.name
+  } isimli rol silindi.`;
 
-	if(!deletionLog) m += `Kimin sildiği bilinmiyor`;
-	else {
-		let { executor, target } = deletionLog;
-		m += `Silen kişi : ${executor.tag}`;
-	};
+  if (!deletionLog) m += `Kimin sildiği bilinmiyor`;
+  else {
+    let { executor, target } = deletionLog;
+    m += `Silen kişi : ${executor.tag}`;
+  }
 
-	db.push("logs",m);
+  db.push("logs", m);
 
-	if(db.get("rolKoruma")){
-		role.guild.roles.create({ name : role.name , reason : "Rol silindi." ,  permissions : role.permissions , color : role.color });
-	};
+  if (db.get("rolKoruma")) {
+    role.guild.roles.create({
+      name: role.name,
+      reason: "Rol silindi.",
+      permissions: role.permissions,
+      color: role.color,
+    });
+  }
 });
 // BOT
 
-
-//SERVER 
-app.get("/isBotLogined",(req,res) => {
-	res.status(200).send({
-		status : (client.user) ? true : false
-	});
+//SERVER
+app.get("/isBotLogined", (req, res) => {
+  res.status(200).send({
+    status: client.user ? true : false,
+  });
 });
 
-app.get("/getInfo",(req,res) => {
-	res.status(200).send({
-		info : {
-			kufurEngel : db.get("kufurEngel") || false,
-			reklamEngel : db.get("reklamEngel") || false,
-			kanalKoruma : db.get("kanalKoruma") || false,
-			rolKoruma : db.get("rolKoruma") || false,
-			logs : db.get("logs") || [],
-			channels : client.guilds.cache.get(process.env.guild).channels.cache.map(r => r).filter(c => c.type == "GUILD_TEXT").filter(c => c.guild.me.permissionsIn(c.id).toArray().includes("MANAGE_CHANNELS")),
-			members : client.guilds.cache.get(process.env.guild).members.cache,
-			guild : process.env.guild,
-			token : process.env.token
-		}
-	});
+app.get("/getInfo", (req, res) => {
+  res.status(200).send({
+    info: {
+      kufurEngel: db.get("kufurEngel") || false,
+      reklamEngel: db.get("reklamEngel") || false,
+      kanalKoruma: db.get("kanalKoruma") || false,
+      rolKoruma: db.get("rolKoruma") || false,
+      logs: db.get("logs") || [],
+      channels: client.guilds.cache
+        .get(process.env.guild)
+        .channels.cache.map((r) => r)
+        .filter((c) => c.type == "GUILD_TEXT")
+        .filter((c) =>
+          c.guild.me.permissionsIn(c.id).toArray().includes("MANAGE_CHANNELS")
+        ),
+      members: client.guilds.cache.get(process.env.guild).members.cache,
+      guild: process.env.guild,
+      token: process.env.token,
+    },
+  });
 });
 
-app.get("/changeKufurEngel",(req,res) => {
-	let k ;
+app.get("/changeKufurEngel", (req, res) => {
+  let k;
 
-	if(db.get("kufurEngel")) k = !db.get("kufurEngel");
-	else k = true;
+  if (db.get("kufurEngel")) k = !db.get("kufurEngel");
+  else k = true;
 
-	db.set("kufurEngel",k);
+  db.set("kufurEngel", k);
 
-	res.status(200).send();
+  res.status(200).send();
 });
 
-app.get("/changeReklamEngel",(req,res) => {
-	let k ;
+app.get("/changeReklamEngel", (req, res) => {
+  let k;
 
-	if(db.get("reklamEngel")) k = !db.get("reklamEngel");
-	else k = true;
+  if (db.get("reklamEngel")) k = !db.get("reklamEngel");
+  else k = true;
 
-	db.set("reklamEngel",k);
+  db.set("reklamEngel", k);
 
-	res.status(200).send();
+  res.status(200).send();
 });
 
-app.get("/changeKanalKoruma",(req,res) => {
-	let k ;
+app.get("/changeKanalKoruma", (req, res) => {
+  let k;
 
-	if(db.get("kanalKoruma")) k = !db.get("kanalKoruma");
-	else k = true;
+  if (db.get("kanalKoruma")) k = !db.get("kanalKoruma");
+  else k = true;
 
-	db.set("kanalKoruma",k);
+  db.set("kanalKoruma", k);
 
-	res.status(200).send();
+  res.status(200).send();
 });
 
-app.get("/changeRolKoruma",(req,res) => {
-	let k ;
+app.get("/changeRolKoruma", (req, res) => {
+  let k;
 
-	if(db.get("rolKoruma")) k = !db.get("rolKoruma");
-	else k = true;
+  if (db.get("rolKoruma")) k = !db.get("rolKoruma");
+  else k = true;
 
-	db.set("rolKoruma",k);
+  db.set("rolKoruma", k);
 
-	res.status(200).send();
+  res.status(200).send();
 });
 
-app.post("/setSlowMode",async (req,res) => {
-	let { channel , time } = req.body;
+app.post("/setSlowMode", async (req, res) => {
+  let { channel, time } = req.body;
 
-	try {
-		await client.channels.cache.get(channel).setRateLimitPerUser(parseInt(time), "Yavaş Mod");
-		db.push("logs",`${String(new Date()).split(" ")[4]} ${channel} ID'li kanala ${time} saniye yavaş mod uygulandı.`);
-		res.status(200).send({ status : "Başarılı" });
-	}catch(err){
-		res.status(200).send({ status : "Bir hata oluştu. Konsolda görebilirsiniz" , err});
-	};
+  try {
+    await client.channels.cache
+      .get(channel)
+      .setRateLimitPerUser(parseInt(time), "Yavaş Mod");
+    db.push(
+      "logs",
+      `${
+        String(new Date()).split(" ")[4]
+      } ${channel} ID'li kanala ${time} saniye yavaş mod uygulandı.`
+    );
+    res.status(200).send({ status: "Başarılı" });
+  } catch (err) {
+    res
+      .status(200)
+      .send({ status: "Bir hata oluştu. Konsolda görebilirsiniz", err });
+  }
 });
 
-app.post("/setTimeOut",async(req,res) => {
-	let { user , time } = req.body;
+app.post("/setTimeOut", async (req, res) => {
+  let { user, time } = req.body;
 
-	try {
-		await client.guilds.cache.get(process.env.guild).members.cache.get(user).timeout(parseInt(time),'Zaman Aşımı');
-		res.status(200).send({ status : "Başarılı" });
-	}catch(err){
-		res.status(200).send({ status : "Bir hata oluştu. Konsolda görebilirsiniz" , err});
-	}
+  try {
+    await client.guilds.cache
+      .get(process.env.guild)
+      .members.cache.get(user)
+      .timeout(parseInt(time), "Zaman Aşımı");
+    res.status(200).send({ status: "Başarılı" });
+  } catch (err) {
+    res
+      .status(200)
+      .send({ status: "Bir hata oluştu. Konsolda görebilirsiniz", err });
+  }
 });
-//SERVER 
-
+//SERVER
 
 app.listen(process.env.PORT);
